b4026aad9b9a3cb51c2dc4e041ec9d93298ff434
Evalou <elouvaekpao@gmail.com>
2026-06-11 15:00:05 +0000

    Cors ajouté

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express()
 //Configuration pour utiliser le json  
 app.use(express.json())
 app.use(express.urlencoded({ extended : false }))
-app.use(cors())
+app.use(cors(
+    {
+    origin: 'https://lucent-sable-4ec864.netlify.app',
+    credentials: true
+}
+))
 
 app.use('/v1/tache' , require("./routes/tache") )
  
